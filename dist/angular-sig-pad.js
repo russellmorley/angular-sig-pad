95fc5e18fc722625aefc3a5567d68da977474f9d
mackenzie2 <me@mackenzie2.local>
2015-08-06 17:15:21 -0700

    Testing and bug fixing

--- dist/angular-sig-pad.js
+++ dist/angular-sig-pad.js
@@ -22,7 +22,7 @@
               link: function (scope, element, attrs, ngModel) {
                   if (!ngModel) return;  //require ngModel on element associated with this attribute, otherwise there is nothing for this directive to do.
                   
-                  var sigPad = new $window.SignaturePad($element[0]); //element should be a canvas
+                  var sigPad = new $window.SignaturePad(element[0]); //element should be a canvas
 
                   //specify how the UI should be updated
                   ngModel.$render = function() { // called when the value referenced by ng-model is changed programmatically and both $modelValue and $viewValue are
@@ -45,31 +45,31 @@
                   }
 
                   $timeout(function () { // get the other attributes once DOM has finished rendering
-                      if (!attrs.angularSigPadDotSize) {
+                      if (attrs.angularSigPadDotSize) {
                           sigPad.dotSize = attrs.angularSigPadDotSize;
                       }
 
-                      if (!attrs.angularSigPadinWidth) {
+                      if (attrs.angularSigPadinWidth) {
                           sigPad.minWidth = attrs.angularSigPadMinWidth;
                       }
 
-                      if (!attrs.angularSigPadMaxWidth) {
+                      if !attrs.angularSigPadMaxWidth) {
                           sigPad.maxWidth = attrs.angularSigPadMaxWidth;
                       }
 
-                      if (!attrs.angularSigPadBackgroundColor) {
+                      if (attrs.angularSigPadBackgroundColor) {
                           sigPad.backgroundColor = attrs.angularSigPadBackgroundColor;
                       }
 
-                      if (!attrs.angularSigPadPenColor) {
+                      if (attrs.angularSigPadPenColor) {
                           sigPad.penColor = attrs.angularSigPadPenColor;
                       }
 
-                      if (!attrs.angularSigPadVelocityFilterWeight) {
+                      if (attrs.angularSigPadVelocityFilterWeight) {
                           sigPad.velocityFilterWeight = attrs.angularSigPadVelocityFilterWeight;
                       }
 
-                      if (!attrs.angularSigPadPadOff) {
+                      if (attrs.angularSigPadPadOff) {
                           if (attrs.ngSignaturePadOff === 'true') {
                               sigPad.off();
                           }
